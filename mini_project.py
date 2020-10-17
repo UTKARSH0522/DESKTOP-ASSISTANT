3f37feae3656903d37415936dfc7e6c257ba9885
UTKARSH0522 <36822038+UTKARSH0522@users.noreply.github.com>
2020-10-17 10:53:01 -0700

    Update mini_project.py

--- mini_project.py
+++ mini_project.py
@@ -29,4 +29,15 @@ def wishMe():
         print("Listening...")
         r.pause_threshold = 1
         audio = r.listen(source)
+         try:
+        print("Recognizing...")    
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        # print(e)    
+        print("Say that again please...")  
+        return "None"
+    return query
+
         
