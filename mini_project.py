5d6baff9c4ac705fae94c589c11e4c8fe453c717
UTKARSH0522 <36822038+UTKARSH0522@users.noreply.github.com>
2020-10-17 10:50:23 -0700

    Update mini_project.py

--- mini_project.py
+++ mini_project.py
@@ -12,3 +12,7 @@ engine.setProperty('voice', voices[0].id)
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning!")
