c883869dc93342728dcac7a1254be477a9da241d
UTKARSH0522 <36822038+UTKARSH0522@users.noreply.github.com>
2020-10-17 11:02:47 -0700

    Update mini_project.py

--- mini_project.py
+++ mini_project.py
@@ -73,5 +73,7 @@ def sendEmail(to, content):
              elif 'the time' in query:
             strTime = datetime.datetime.now().strftime("%H:%M:%S")    
             speak(f"Sir, the time is {strTime}")
-            
+            elif 'open code' in query:
+            codePath = "C:\\Users\\UTKARSH\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+            os.startfile(codePath)
         
