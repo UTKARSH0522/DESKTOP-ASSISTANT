3f19117c83d59db6a4f67e98eb3cb1897ec33043
UTKARSH0522 <36822038+UTKARSH0522@users.noreply.github.com>
2020-10-17 10:59:33 -0700

    Update mini_project.py

--- mini_project.py
+++ mini_project.py
@@ -61,5 +61,8 @@ def sendEmail(to, content):
             speak(results)
              elif 'open youtube' in query:
             webbrowser.open("youtube.com")
+            elif 'open google' in query:
+            webbrowser.open("google.com")
+            
             
         
