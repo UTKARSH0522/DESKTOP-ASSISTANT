1871a6a89ae5e9cfd4813e6d84d5b33586c592fa
UTKARSH0522 <36822038+UTKARSH0522@users.noreply.github.com>
2020-10-17 11:00:10 -0700

    Update mini_project.py

--- mini_project.py
+++ mini_project.py
@@ -63,6 +63,7 @@ def sendEmail(to, content):
             webbrowser.open("youtube.com")
             elif 'open google' in query:
             webbrowser.open("google.com")
-            
+            elif 'open stackoverflow' in query:
+            webbrowser.open("stackoverflow.com")   
             
         
